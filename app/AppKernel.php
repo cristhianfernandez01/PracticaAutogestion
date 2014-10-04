e8e1c67e782913a26d8b2a2b5b3171945e8f6f47
cristhianfernandez01 <cristhian.fernandez01@gmail.com>
2014-10-04 12:27:20 -0300

    Version 3

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Diloog\AfiliadoBundle\AfiliadoBundle(),
             new Diloog\BackendBundle\BackendBundle(),
+            new Diloog\PagoBundle\PagoBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
